91ac73bdce66352e64b85ef3d34d2789604903f9
sydcameron <scameron@wesleyan.edu>
2025-10-15 09:00:21 -0400

    Penguins Summary

---
+++ Penguins.R
@@ -0,0 +1,6 @@
+penguins <- read.csv("~/Documents/GitHub/assignment2-SC/penguins.csv")
+summary(penguins$bill_length_mm)
+sd(penguins$bill_length_mm, na.rm = TRUE)
+freq(penguins$sex)
+freq(penguins$flipper_length_mm)
+
