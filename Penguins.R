a0eeb6247ab89a6b1cfad1483010d602e20e7c97
kmai-spec <kmai@wesleyan.edu>
2025-10-15 09:32:23 -0400

    Updated Sydney's code with my script
    Updated Sydney's code with my script

--- Penguins.R
+++ Penguins.R
@@ -4,3 +4,8 @@ sd(penguins$bill_length_mm, na.rm = TRUE)
 freq(penguins$sex)
 freq(penguins$flipper_length_mm)
 
+#Kenny's addition
+my.lm <- lm(bill_length_mm ~ factor(sex), data = data) 
+summary(my.lm)
+
+
